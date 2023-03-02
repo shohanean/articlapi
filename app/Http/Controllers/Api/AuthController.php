8ce989e7a4c1a193a8d2b1ca3ea63109a5a82e88
shohanean <hossain_shohan1@live.com>
2023-03-02 16:21:29 +0600

    login auth

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
@@ -26,6 +27,24 @@ class AuthController extends Controller
             $success['token'] = $user->createToken($user->id)->plainTextToken;
             $success['user'] = $user;
             return response()->json($success, 201);
+        }
+    }
+    function login(Request $request){
+        $validator =  Validator::make($request->all(), [
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }else{
+            if (Auth::attempt($request->all())) {
+                $success['token'] = Auth::user()->createToken(Auth::user()->id)->plainTextToken;
+                $success['user'] = Auth::user();
+                return response()->json($success, 201);
+            } else {
+                return response()->json('Email or password is wrong');
+            }
         }
     }
 }
